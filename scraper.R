4ecf57c10357aa1c70abf86b6cb03a83d55bda1a
Luis Capelo <luiscape@gmail.com>
2014-05-23 14:32:17 -0400

    fixing typo

--- scraper.R
+++ scraper.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 ## This tracker was written to create an elementary analytics
 ## reporting system for a CKAN instance (data.hdx.rwlabs.org).
 ## The tracker queries the API every hour and records the resulting number
@@ -44,29 +46,29 @@ number_of_tags <- nrow(data.frame(fromJSON(getURL(num_tags_url, userpwd = base_k
 # Number of licenses
 num_licenses_url <- 'http://data.hdx.rwlabs.org/api/action/license_list'
 licenses_result <- fromJSON(getURL(num_licenses_url, userpwd = base_key))
-number_of_licenses <- length(number_of_licenses[3]$result)
+number_of_licenses <- length(licenses_result[3]$result)
 
 # Collecting date and time.
 date_and_time <- as.character(Sys.time())
 
 # Making a single data.frame.
 hdx_repo_analytics <- data.frame(number_of_datasets, number_of_countries, number_of_organizations, 
-number_of_users, number_of_tags, number_of_licenses, date_and_time)
+                                 number_of_users, number_of_tags, number_of_licenses, date_and_time)
 
 # Adding pretty names.
-names(hdx_repo_analytics) <- c("Number_of_Datasets", "Number_of_Countries", "Number_of_Organizations", "Number_of_Users", "Number_of_Tags", "Number_of_Licenses", "Data_and_Time")
+names(hdx_repo_analytics) <- c("Number_of_Datasets", "Number_of_Countries", "Number_of_Organizations", "Number_of_Users", "Number_of_Tags", "Number_of_Licenses", "Date_and_Time")
 
 # Storing results in a db. 
 cat('Store 2 tables (results and user list) in a database.')
 db <- dbConnect(SQLite(), dbname="scraperwiki.sqlite")
 
-    dbWriteTable(db, "hdx_repo_analytics", hdx_repo_analytics, row.names = FALSE, append = TRUE)
-    dbWriteTable(db, "list_of_users", list_of_users, row.names = FALSE, overwrite = TRUE)
+dbWriteTable(db, "hdx_repo_analytics", hdx_repo_analytics, row.names = FALSE, append = TRUE)
+dbWriteTable(db, "list_of_users", list_of_users, row.names = FALSE, overwrite = TRUE)
 
-    # for testing purposes
-    # dbListTables(db)
-    # x <- dbReadTable(db, "hdx_repo_analytics")
-    # y <- dbReadTable(db, "list_of_users")
+# for testing purposes
+# dbListTables(db)
+# x <- dbReadTable(db, "hdx_repo_analytics")
+# y <- dbReadTable(db, "list_of_users")
 
 dbDisconnect(db)
 cat('done')
